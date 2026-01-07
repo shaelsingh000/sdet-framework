5dc01a02aa1a214a3093275b13b0913c33823e32
Shailendra <shailendra.singh@landisgyr.com>
2026-01-07 13:17:28 +0530

    Fix UI fixture naming and Page Object architecture

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -1,5 +1,5 @@
 from pages.login_page import LoginPage
 
-def test_valid_login(driver):
-    login = LoginPage(driver)
+def test_valid_login(browser):
+    login = LoginPage(browser)
     login.login("admin", "password")
