8ac05f62fcbee8cee4d9a3ddd4703bbc84ecb871
Shailendra <shailendra.singh@landisgyr.com>
2026-01-07 13:01:05 +0530

    Day 18: File upload and dropdown utilities

--- core/browser_action.py
+++ core/browser_action.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 
 class BrowserActions:
     def __init__(self, driver, timeout=15):
