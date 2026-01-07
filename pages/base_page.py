6fbf6579addf5aef154e62310706f47103ea9885
Shailendra <shailendra.singh@landisgyr.com>
2026-01-07 12:53:55 +0530

    Day 16: Proper Page Object Model

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,9 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from core.browser_action import BrowserActions
 
 class BasePage:
     def __init__(self,driver):
         self.driver = driver
-        self.wait = WebDriverWait(driver,10)
+        self.driver = BrowserActions(driver)
         
